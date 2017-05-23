fba7a2a9919a840363c12f553f30f30afbe56835
Eric Windmill <ericwindmill@gmail.com>
2017-05-22 19:53:08 -0700

    finish paralax effect

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -11,43 +11,62 @@ const Home = () => (
 
       <div id="group1" className="parallax-group">
         <div className="parallax__layer parallax__layer--base">
-          <div className="landing-copy landing-grid-a">
+          <div className="landing-copy">
             <div className="landing-grid-a">
-              <h1>A place for mega fans.</h1><h2>Cause you arent a true fan unless you can prove it with metrics.</h2>
+              <h1 className="copy-header">A place for mega fans.</h1>
             </div>
             <Link className="btn landing-btn landing-grid-b" to="/sign-up">Sign Up Today</Link>
+          <div className="landing-grid-c">
           </div>
+         </div>
         </div>
-        <div className="parallax__layer parallax__layer--back">
-          
-        </div>
+        <div className="parallax__layer parallax__layer--back"></div>
       </div>
-
-      <div id="group2" className="parallax-group">
+      <div className="landing-learn-more">
+        <h4 className="landing-learn-more-text">Learn About FansSapce</h4>
+        <i className="fa fa-angle-double-down" aria-hidden="true"></i>
+      </div>
+      <div id="group4" className="parallax-group">
         <div className="parallax__layer parallax__layer--base">
-          <h1 className="copy"> COPY </h1>
-        </div>
-        <div className="parallax__layer parallax__layer--back">
-          <h1 className="copy"> background layer </h1>
+          <h1>Fanspace.io</h1>
+          <h2>Connect</h2>
+          <h3>Geek out with with other giant nerds</h3>
+          <h2>Follow </h2>
+          <h3>Keep track of all the past and future shows.</h3>
+          <h2>Track</h2>
+          <h3>Its 2017! You Need Metrics!</h3>
         </div>
       </div>
 
       <div id="group3" className="parallax-group">
-        <div className="parallax__layer parallax__layer--fore">
-           <h1 className="copy"> MORE COPY </h1>
-        </div>
         <div className="parallax__layer parallax__layer--base">
-          <h1 className="copy"> background </h1>
+           <h1> Metrics </h1>
+           <h2> Learn about the shows youve seen </h2>
+           <p> ** insert metrics pictures </p>
         </div>
+        <div className="parallax__layer parallax__layer--back"></div>
       </div>
+
 
       <div id="group4" className="parallax-group">
         <div className="parallax__layer parallax__layer--base">
-          <h1 className="copy"> background </h1>
+          <h1 className="copy">Its Easy.</h1>
+          <ul>
+            <li>Create an Account</li>
+            <li>Browse the shows</li>
+            <li>Select the shows youve seen</li>
+            <li>See your results!</li>
+          </ul>
         </div>
       </div>
 
-
+      <div id="group3" className="parallax-group">
+        <div className="parallax__layer parallax__layer--base">
+           <h1 className="copy"> Remember: </h1>
+           <h2>You arent a true fan unless you can prove your value!</h2>
+        </div>
+        <div className="parallax__layer parallax__layer--back"></div>
+      </div>
     </div>
   <Footer />
 </div>
