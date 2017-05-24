ebf555f4c8895381e4a05f2a463ca2d61b759bd9
Eric Windmill <ericwindmill@gmail.com>
2017-05-24 09:56:51 -0700

    remove bad migration

--- db/migrate/20170522141923_add_defaults_to_users.rb
+++ db/migrate/20170522141923_add_defaults_to_users.rb
@@ -1,6 +1,6 @@
 class AddDefaultsToUsers < ActiveRecord::Migration[5.0]
   def change
-    change_column: :users, :location, :string, default: "The Milky Way"
-    change_column: :users, :profile_img_url, :string, default: 'assets/user_images/default_user2'
+    
+    
   end
 end
