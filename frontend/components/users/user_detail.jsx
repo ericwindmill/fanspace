c1c9b2c2256aadf248340eae62b92b2270ba15b6
Eric Windmill <ericwindmill@gmail.com>
2017-05-24 17:04:09 -0700

    More style updates

--- frontend/components/users/user_detail.jsx
+++ frontend/components/users/user_detail.jsx
@@ -57,6 +57,8 @@ displayUpdate(e) {
       <div className='profile-detail-main'>
         <LeftNav profile_img={userDetail.profile_img_url} />
         <div className="profile-detail-grid"> 
+
+        <section className="profile-info">
           <div className="profile-detail-column-1">
             <img className="profile-img" src={userDetail.profile_img_url} />
           </div>
@@ -65,36 +67,110 @@ displayUpdate(e) {
             <p className="profile-location"> Location: {userDetail.location}</p>
             <p className="profile-rank" >{userDetail.rank}</p>
             <p className="profile-tagline" >{userDetail.tagline}</p>
-
-            
             {userMatch ? (
-              <button className="update-profile-button" onClick={this.displayUpdate}> {buttonText} </button>
+              <button className="update-profile-button btn" onClick={this.displayUpdate}> {buttonText} </button>
                ) : (
               <div></div> 
              )}
+          </div>
+        </section>
+
+        <Route path="/users/:id/update" render={() => (
+            <UpdateForm
+              history={this.props.history}
+              userDetail={this.props.userDetail}
+              currentUser={this.props.currentUser}
+              updateCurrentUser={this.props.updateCurrentUser} />
+        )} />
+
+          
+            <section className="stats-at-a-glance-table">
+              <h3 className="table-glance-header">Stats At a Glance</h3>
+              <div className="main-shows grid-box">
+                <p className="icon-ticket-1 fontastic"></p>
+                <p>Shows</p>
+                <p>{this.props.userDetail.setlist.length}</p>
+              </div>
+              
+              <div className="main-cities grid-box">
+                <p className="icon-map fontastic"></p>
+                <p >Cities</p>
+                <p>{this.props.uniqCities.length}</p>
+              </div>
+              
+              <div className="main-venues grid-box">
+                <p className="icon-theatre fontastic"></p>
+                <p>Venues</p>
+                <p>{this.props.uniqVenues.length}</p>
+              </div>
+
+              <div className="main-performances grid-box">
+                <p className="icon-music fontastic"></p>
+                <p>Total Songs</p>
+                <p>{this.props.performances.length}</p>
+              </div>
+
+              <div className="main-songs grid-box">
+                <p className="icon-nuclear fontastic"></p>
+                <p>Unique Songs</p>
+                <p>{this.props.uniqSongs.length}</p>
+              </div>
+            </section>
+
+            <section className="album-percentage-widget">
+              <h3> Album Percentages </h3>
+              <ul>
+                {this.props.albumPercent.map(album => {
+                  return <li className="album-percentage-item" key={album.title}> 
+                  <div className="col-1">{album.title}</div> 
+                  <div className="col-2">{album.percent}%</div>
+                  </li>
+                })}
+              </ul>
+            </section>
+
+
+            <section className="shows-per-year-widget">
+              <h3> Shows Per Year </h3>
+              <ul>
+                {this.props.showsPerYear.map(year => {
+                  return <li className="shows-per-year-item" key={year.year}>
+                    <div className="col-1">{year.year}</div> 
+                    <div className="col-2">{year.count}</div>
+                  </li>
+                })}
+              </ul>
+            </section>
+          
+
+          <section className="user-setlists">
+            <h3> Shows Attended </h3>
+            <ul>
+              {this.props.userDetail.setlist.map(set => {
+                return (
+                  <li className="user-setlist-item" key={set.id}>
+                    <Link to={`/setlists/${set.id}`}>{set.date} {set.city}</Link>
+                    <img className="user-setlist-item-poster" src={set.poster_img_url} />
+                  </li>
+                )
+              })}
+            </ul>
+          </section> 
+
+        <section className="uniq-songs-by-album">
+          <ul>
+            {this.props.uniqSongsByAlbum.map(album => {
+              return <li className="uniq-songs-album-item" key={album.title}> {album.title} : 
             
-          </div>
-            <Route path="/users/:id/update" render={() => (
-                <UpdateForm
-                  history={this.props.history}
-                  userDetail={this.props.userDetail}
-                  currentUser={this.props.currentUser}
-                  updateCurrentUser={this.props.updateCurrentUser} />
-            )} />
-          <div className="metrics-main profile-detail-grid-4">
-            <UserMetrics 
-                history={this.props.history}
-                userDetail={this.props.userDetail}
-                currentUser={this.props.currentUser}
-                uniqCities={this.props.uniqCities}
-                uniqVenues={this.props.uniqVenues}
-                performances={this.props.performances}
-                uniqSongs={this.props.uniqSongs}
-                albumPercent={this.props.albumPercent}
-                showsPerYear={this.props.showsPerYear}
-                uniqSongsByAlbum={this.props.uniqSongsByAlbum}
-            />
-          </div>
+              <ul key={album.title}>
+                {album.songs.map(song => {
+                return <li key={album.key}> {song} </li> })}
+              </ul>
+
+            </li>
+           })}
+          </ul>
+        </section>
 
         </div>
       </div>
